f65ca5c25646de1b601103d0c949eaa3a8711cec
LeonardoSantos97 <leonardopsantos97@gmail.com>
2022-02-08 20:06:45 -0300

    Created endpoint for delete admin

--- src/main/java/com/leoanrdo/manager_book/resource/AdminResource.java
+++ src/main/java/com/leoanrdo/manager_book/resource/AdminResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -52,5 +53,11 @@ public class AdminResource {
 	public ResponseEntity<AdminDTO> update(@PathVariable Integer id, @Valid @RequestBody AdminDTO objDTO){
 		Admin Obj = service.update(id, objDTO);
 		return ResponseEntity.ok().body(new AdminDTO(Obj));
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<AdminDTO> delete(@PathVariable Integer id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/leoanrdo/manager_book/services/AdminService.java
+++ src/main/java/com/leoanrdo/manager_book/services/AdminService.java
@@ -16,18 +16,15 @@ import com.leoanrdo.manager_book.repositories.PessoaRepository;
 import com.leoanrdo.manager_book.services.exceptions.DataIntegrityViolationException;
 import com.leoanrdo.manager_book.services.exceptions.ObjectnotFoundExceptions;
 
-import javassist.tools.rmi.ObjectNotFoundException;
-
 @Service
 public class AdminService {
 
-	
 	@Autowired
 	private AdminRepository repository;
-	
+
 	@Autowired
 	private PessoaRepository pessoaRepository;
-	
+
 	public Admin findById(Integer id) {
 		Optional<Admin> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectnotFoundExceptions("Não encontrado! ID: " + id));
@@ -43,21 +40,29 @@ public class AdminService {
 		Admin newObj = new Admin(objDTO);
 		return repository.save(newObj);
 	}
-	
+
 	public Admin update(Integer id, @Valid AdminDTO objDTO) {
 		objDTO.setId(id);
-		Admin oldObj =  findById(id);
+		Admin oldObj = findById(id);
 		validaPorUsuario(objDTO);
 		oldObj = new Admin(objDTO);
 		return repository.save(oldObj);
 	}
 
+	public void delete(Integer id) {
+		Admin obj = findById(id);
+		if (obj.getLivros().size() > 0) {
+			throw new DataIntegrityViolationException("O Admin possui livros e não pode ser deletado!");
+		}
+		repository.deleteById(id);
+
+	}
+
 	private void validaPorUsuario(AdminDTO objDTO) {
-		Optional<Pessoa> obj = pessoaRepository.findByUsuario(objDTO.getUsuario()); 
-		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+		Optional<Pessoa> obj = pessoaRepository.findByUsuario(objDTO.getUsuario());
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
 			throw new DataIntegrityViolationException("Usuário já cadastrado!");
 		}
 	}
 
-	
 }
