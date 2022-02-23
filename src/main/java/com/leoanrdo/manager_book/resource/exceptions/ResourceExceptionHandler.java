93e1b21d798bfd49deeec9e72870059ec84a84eb
LeonardoSantos97 <leonardopsantos97@gmail.com>
2022-02-23 18:17:46 -0300

    Add exceptions

--- src/main/java/com/leoanrdo/manager_book/resource/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/leoanrdo/manager_book/resource/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -14,22 +16,36 @@ import com.leoanrdo.manager_book.services.exceptions.ObjectnotFoundExceptions;
 public class ResourceExceptionHandler {
 
 	@ExceptionHandler(ObjectnotFoundExceptions.class)
-	public ResponseEntity<StandardError> objectnotFoundExceptions(ObjectnotFoundExceptions ex,
-			HttpServletRequest request){
-		
+	public ResponseEntity<StandardError> objectnotFoundException(ObjectnotFoundExceptions ex,
+			HttpServletRequest request) {
+
 		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
 				"Object Not Found", ex.getMessage(), request.getRequestURI());
-		
+
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 	}
-	
+
 	@ExceptionHandler(DataIntegrityViolationException.class)
 	public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex,
-			HttpServletRequest request){
-		
+			HttpServletRequest request) {
+
 		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
-				"Violação de Dados", ex.getMessage(), request.getRequestURI());
-		
+				"Violação de dados", ex.getMessage(), request.getRequestURI());
+
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validationErrors(MethodArgumentNotValidException ex,
+			HttpServletRequest request) {
+
+		ValidationError errors = new ValidationError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+				"Validation error", "Erro na validação dos campos", request.getRequestURI());
+
+		for (FieldError x : ex.getBindingResult().getFieldErrors()) {
+			errors.addError(x.getField(), x.getDefaultMessage());
+		}
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
 	}
 }
